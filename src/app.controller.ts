63a0ddf42af5888689c806b3e22ec6c5da8b92de
Insthync <ittipon.bay@gmail.com>
2021-09-27 01:14:15 +0700

    Add skeleton functions

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,65 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('login')
+  async login(@Request() request) {
+
+  }
+
+  @Post('login-with-facebook')
+  async loginWithFacebook(@Request() request) {
+
+  }
+
+  @Post('link-with-facebook')
+  async linkWithFacebook(@Request() request) {
+
+  }
+
+  @Post('unlink-with-facebook')
+  async unlinkWithFacebook(@Request() request) {
+
+  }
+
+  @Post('login-with-google')
+  async loginWithGoogle(@Request() request) {
+
+  }
+
+  @Post('link-with-google')
+  async linkWithGoogle(@Request() request) {
+
+  }
+
+  @Post('unlink-with-google')
+  async unlinkWithGoogle(@Request() request) {
+
+  }
+
+  @Post('logout')
+  async logout(@Request() request) {
+
+  }
+
+  @Post('change-password')
+  async changePassword(@Request() request) {
+    
+  }
+
+  @Post('register')
+  async register(@Request() request) {
+    
+  }
+
+  @Post('refresh-token')
+  async refreshToken(@Request() request) {
+    
+  }
+
+  @Post('verify-token')
+  async verifyToken(@Request() request) {
+    
   }
 }
